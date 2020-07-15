7206fdc45450457f33854e367661be9a00f19cad
Lucas Machado <lucas.machado@bs2.com>
2020-07-15 09:56:23 -0300

    Adição dos menus.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
+import frontBack from './back_front.jpg'
 import './App.css';
+import './Bem.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+      <div className="div-center">
+          <ul className="menu-horizontal">
+            <li className="menu-horizontal-nav"><a className="menu-horizontal-nav-item" href="default.asp">Home</a></li>
+            <li className="menu-horizontal-nav"><a className="menu-horizontal-nav-item" href="news.asp">News</a></li>
+            <li className="menu-horizontal-nav"><a className="menu-horizontal-nav-item" href="contact.asp">Contact</a></li>
+            <li className="menu-horizontal-nav"><a className="menu-horizontal-nav-item" href="about.asp">About</a></li>
+          </ul>
+          <div className="abstract">
+            <h1 className="abstract-title">Lucas Vinícius Machado</h1>
+            <p className="abstract-p">
+              Sou desenvolvedor de software back-end. Front-end é muito top, mas infelizente não nasci para isso. rsrsrs
+              brincadeirinha. kkkk
+            </p>
+          </div>
+          <div className="abstract">
+            <div className="center">
+              <img className="img-small" src={frontBack}></img>
+            </div>
+          </div>
+      </div>
+      /* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hello World!!!
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -18,8 +40,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </header> */
   );
 }
 
